28059671071fe10845606a3df8d1b908ef375a68
JoseGavilan1 <gavilanhidalgojose@gmail.com>
2024-02-26 22:45:52 -0300

    primer commit

---
+++ .angular/cache/17.1.0/vite/deps/@angular_platform-browser_animations.js
@@ -0,0 +1,21 @@
+import {
+  BrowserAnimationsModule,
+  InjectableAnimationEngine,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations
+} from "./chunk-5UKLCV6O.js";
+import "./chunk-3GCPIP4N.js";
+import "./chunk-2VURXX3Z.js";
+import {
+  ANIMATION_MODULE_TYPE
+} from "./chunk-LTPMASOC.js";
+export {
+  ANIMATION_MODULE_TYPE,
+  BrowserAnimationsModule,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations,
+  InjectableAnimationEngine as ɵInjectableAnimationEngine
+};
+//# sourceMappingURL=@angular_platform-browser_animations.js.map
